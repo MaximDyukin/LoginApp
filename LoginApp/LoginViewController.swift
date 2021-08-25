99bb32793f3b19dd88a5b7587ea39c123f6bd9d9
Max <maximdyukin@gmail.com>
2021-08-26 00:26:17 +0500

    At "the TFUsername" changed Return key to Next. At "the TFPassword" changed Return key to Done. Also there was made the button Done not active while the text entry area contains no text.

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -7,13 +7,20 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var usernameTF: UITextField!
     @IBOutlet weak var passwordTF: UITextField!
     
     private let username = "User"
     private let password = "Password"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        usernameTF.delegate = self
+        passwordTF.delegate = self
+    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
@@ -31,6 +38,7 @@ class LoginViewController: UIViewController {
     
     @IBAction func logining() {
         if usernameTF.text == username && passwordTF.text == password {
+            // Я так и не понял что мне сюда написать, чтобы этот метод срабатывал при нажатии на Done
         } else {
             showAlert(title: "Invalid login or password", message: "Please enter correct login and password") { _ in
                 self.passwordTF.text = ""
@@ -44,5 +52,15 @@ class LoginViewController: UIViewController {
     
     @IBAction func promptingPassword() {
         showAlert(title: "Oops!", message: "Your password is \(password) 😉", handler: nil)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if usernameTF.isFirstResponder {
+            passwordTF.becomeFirstResponder()
+        } else if passwordTF.isFirstResponder {
+            logining()
+        }
+        return true
     }
 }
