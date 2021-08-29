b2fdfd5bcfeb0d4ad963baabaea393a0106d8655
Max <maximdyukin@gmail.com>
2021-08-30 01:54:22 +0500

    Data models were created.

---
+++ LoginApp/Models/PersonData.swift
@@ -0,0 +1,19 @@
+//
+//  PersonData.swift
+//  LoginApp
+//
+//  Created by Maxim Dukin on 30.08.2021.
+//
+
+struct User {
+    let username = "User"
+    let password = "Password"
+    let person = Person()
+}
+
+struct Person {
+    let firstName = "Maxim"
+    let lastName = "Dyukin"
+    let age = "30"
+    let aboutMe = "I was born, grew up and live in the city of Kurgan. While I was at school, I attended various clubs. At first I was engaged in football, and then volleyball. In parallel, I studied at an art school. At the institute, I continued to play volleyball, and I was also often the host of events. At the moment, I work as a bartender."
+}
