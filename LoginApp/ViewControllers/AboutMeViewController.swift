4d757f9a95673fac591ffe977eb3aef4958994c3
Max <maximdyukin@gmail.com>
2021-08-30 02:18:17 +0500

    Additional screens were created. The AboutMeVC class was also created.

---
+++ LoginApp/ViewControllers/AboutMeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AboutMeViewController.swift
+//  LoginApp
+//
+//  Created by Maxim Dukin on 30.08.2021.
+//
+
+import UIKit
+
+class AboutMeViewController: UIViewController {
+    
+    @IBOutlet weak var firstName: UILabel!
+    @IBOutlet weak var lastName: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var aboutMeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
